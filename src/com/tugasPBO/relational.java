38d60366073fec9c41b0a0485e280971e25a01d9
unknown <argya.programmer@gmail.com>
2020-10-17 14:41:42 +0700

    Initial Commit

---
+++ src/com/tugasPBO/relational.java
@@ -0,0 +1,20 @@
+//latihan 13 PBO
+// Nama       : Argya Aulia Fauzandika
+// NIM        : 10119035
+// Kelas      : IF-1
+// Deskripsi  : menampilkan true atau false dari bilangan memakai operator relasi
+package com.tugasPBO;
+
+public class relational {
+    public static void main(String[] args){
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = "+ (bil1 == bil2));
+        System.out.println("a != b = "+ (bil1 != bil2));
+        System.out.println("a > b = "+(bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a ="+(bil2 >= bil1));
+        System.out.println("b <= a ="+(bil2 <= bil1));
+    }
+
+}
